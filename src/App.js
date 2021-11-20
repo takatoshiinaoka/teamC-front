178d3895b3893810e735f15b8aa88218e0680712
Yuki326 <s20a1035@gmail.com>
2021-11-20 10:26:31 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import axios from "axios";
+
 
 function App() {
+  const [text, setText] = useState("0");
+
+  const handleClick = () => {
+    axios.get("http://127.0.0.1:8000/test?id="+text).then((res) => {
+      console.log(text);
+      console.log(res);
+      setText(res.data);
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" onClick={handleClick}>
+          PUSH!!
+        </button>
+        <h1>{text}</h1>
       </header>
     </div>
   );
